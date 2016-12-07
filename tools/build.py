c766b0bfd9f75f8d28a4902808fcfdae4fe799f5
BaerMitUmlaut <BaerMitUmlaut@users.noreply.github.com>
2016-12-07 16:15:04 +0100

    Added build.py with armake

---
+++ tools/build.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+import sys
+
+def main():
+    projectroot = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    componentspath = os.path.join(projectroot, "addons")
+    buildpath = componentspath
+
+    for component in os.listdir(componentspath):
+        if os.path.isdir(os.path.join(componentspath, component)):
+            try:
+                subprocess.check_output([
+                    "armake",
+                    "build",
+                    "-f",
+                    "-p",
+                    "-x",
+                    "*.md",
+                    os.path.join(componentspath, component),
+                    os.path.join(buildpath, "soil_{}.pbo".format(component.lower()))
+                ], stderr = subprocess.STDOUT)
+            except:
+                print("Failed to build {}.".format(component))
+            else:
+                print("Successfully built {}.".format(component))
+
+if __name__ == "__main__":
+    sys.exit(main())
