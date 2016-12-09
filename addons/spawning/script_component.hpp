8b6c1b1973ea80b90fe7fea0bd2013cab50ac419
JP <soldia@gmx.de>
2016-12-09 08:45:34 +0100

    spawning component structure added

---
+++ addons/spawning/script_component.hpp
@@ -0,0 +1,12 @@
+#define COMPONENT spawning
+#define COMPONENT_BEAUTIFIED Spawning
+#include "\x\soil\addons\core\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+
+#ifdef DEBUG_ENABLED_TEMPLATE
+    #define DEBUG_MODE_FULL
+#endif
+
+#include "\x\soil\addons\core\script_macros.hpp"
