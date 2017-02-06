f98ec9f4bbe29f6c80e3d73fac55f798639e8172
BaerMitUmlaut <BaerMitUmlaut@users.noreply.github.com>
2017-02-06 20:44:29 +0100

    Added make script

---
+++ tools/make.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+import sys
+
+def main():
+    projectroot = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    componentspath = os.path.join(projectroot, "addons")
+    buildpath = os.path.join(projectroot, "build")
+
+    if os.path.isdir(buildpath):
+        for file in os.listdir(buildpath):
+            if file[-4:] == ".pbo":
+                os.unlink(os.path.join(buildpath, file))
+    else:
+        os.mkdir(buildpath)
+
+    for component in os.listdir(componentspath):
+        if os.path.isdir(os.path.join(componentspath, component)):
+            if component.startswith(".") or not os.listdir(os.path.join(componentspath, component)):
+                print("  Skipping {}".format(component))
+                continue
+            try:
+                subprocess.check_output([
+                    "armake",
+                    "build",
+                    "-f",
+                    "-x",
+                    "*.md",
+                    "-i",
+                    "../include",
+                    "-i",
+                    "..",
+                    os.path.join(componentspath, component),
+                    os.path.join(buildpath, "soil_{}.pbo".format(component.lower()))
+                ], stderr = subprocess.STDOUT)
+            except:
+                print("  Failed to build {}".format(component))
+            else:
+                print("  Successfully built {}".format(component))
+
+if __name__ == "__main__":
+    sys.exit(main())
